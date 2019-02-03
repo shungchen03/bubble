088e54b15148b1764c46cc09755fd02aed971040
shung <shungchen03@gmail.com>
2019-02-03 15:58:44 -0500

    update message 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld{
     /*
     Use System.out.println() to print on console.
     */
-    System.out.println("Hello World !");
+    System.out.println("Hello World 1 !");
   
   }
  
